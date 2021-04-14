1f79f16edbbe269ce20e21cb8db35f6f70d4b686
ZacharyNicolai1 <77758631+ZacharyNicolai1@users.noreply.github.com>
2021-04-14 02:43:07 -0400

    Update Invoice.java

--- src/Invoice/Invoice.java
+++ src/Invoice/Invoice.java
@@ -1,4 +1,41 @@
 package src.Invoice;
 
+
+import src.Item.Items;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Vector;
+
 public class Invoice {
+    //header
+    private String invoiceDate;
+    private double totalInvoiceAmount;
+
+    //details
+    //items?
+    //might not need
+    private String orderDate; //header
+    private int customerOrderNumber; //header
+
+    //constructors
+    public Invoice(){
+        invoiceDate ="";
+
+    }
+    /*
+    public Invoice() {
+    }
+*/
+    //set methods
+
+    //get methods
+
+
+    //toString() override which displays Item profile details
+    @Override
+    public String toString(){
+        DecimalFormat df = new DecimalFormat("#.00");
+        return "";
+    }
 }
