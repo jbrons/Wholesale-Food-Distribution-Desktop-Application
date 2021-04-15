11f726f7193f6a10991bf8df2d910b1169d4f4d5
ZacharyNicolai1 <77758631+ZacharyNicolai1@users.noreply.github.com>
2021-04-14 21:02:30 -0400

    worked on invoice GUIS

--- GUI/InvoiceManagement/InvoiceDisplayGUI.java
+++ GUI/InvoiceManagement/InvoiceDisplayGUI.java
@@ -1,4 +1,10 @@
 package GUI.InvoiceManagement;
 
+import javax.swing.*;
+
 public class InvoiceDisplayGUI {
+    private JPanel rootPanel;
+    private JPanel invoicePanel;
+    private JPanel headerPanel;
+    private JPanel detailPanel;
 }
--- GUI/InvoiceManagement/InvoiceGUI.java
+++ GUI/InvoiceManagement/InvoiceGUI.java
@@ -1,7 +1,28 @@
 package GUI.InvoiceManagement;
 
 import javax.swing.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
-public class InvoiceGUI {
+public class InvoiceGUI implements FocusListener {
     private JPanel rootPanel;
+    private JTextField searchField;
+    private JList iList;
+    private JButton searchButton;
+    private JButton leaveButton;
+    private JButton logoutButton;
+    private JLabel invoiceLabel;
+    private JButton createInvoiceButton;
+    private JTextField focused = searchField;
+
+
+
+    //methods required for implementing focusListener
+    public void focusGained(FocusEvent e) {
+        if (e.getSource() instanceof JTextField) {
+            focused = (JTextField) e.getSource();
+        }
+    }
+    public void focusLost(FocusEvent e) {
+    }
 }
