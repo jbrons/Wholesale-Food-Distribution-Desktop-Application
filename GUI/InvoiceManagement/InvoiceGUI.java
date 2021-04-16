8e4a95b8f21e6d2151472816e43728ae23236e72
ZacharyNicolai1 <77758631+ZacharyNicolai1@users.noreply.github.com>
2021-04-16 04:22:44 -0400

    Worked on feature #7

--- GUI/InvoiceManagement/InvoiceGUI.java
+++ GUI/InvoiceManagement/InvoiceGUI.java
@@ -1,8 +1,22 @@
 package GUI.InvoiceManagement;
 
+import GUI.ItemManagement.AddItemsGUI;
+import GUI.ItemManagement.ItemDisplayGUI;
+import GUI.Login.LoginGUI;
+import GUI.MainMenu.MainMenuGUI;
+import GUI.MainWindow.MainWindowGUI;
+import src.Customer.CustomerProfileDatabase;
+import src.Customer.CustomerProfile;
+import src.Customer.CustomerProfileDatabase;
+import src.CustomerOrder.CustomerOrder;
+import src.CustomerOrder.CustomerOrderDatabase;
+import src.Invoice.InvoiceList;
+import src.User.EnumUserRoles;
+
 import javax.swing.*;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Vector;
 
 public class InvoiceGUI implements FocusListener {
     private JPanel rootPanel;
@@ -14,9 +28,82 @@ public class InvoiceGUI implements FocusListener {
     private JLabel invoiceLabel;
     private JButton createInvoiceButton;
     private JTextField focused = searchField;
+    private MainWindowGUI mainWindowGUI;
+
+    InvoiceList invoiceList = InvoiceList.getInstance();
+    CustomerProfileDatabase customerProfileDatabase = CustomerProfileDatabase.getInstance();
+    CustomerOrderDatabase customerOrderDatabase = CustomerOrderDatabase.getInstance();
 
 
+    public InvoiceGUI(){
+        mainWindowGUI = MainWindowGUI.getInstance();
+        setupGUI();
+    }
 
+    public void setupGUI()
+    {
+        //button actions
+        searchButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                /*
+                    String searchedItem = searchField.getText();
+                    setCatalog(itemsList.getSearchDetails(searchedItem));
+                    if(iList.getModel().getSize() == 0 ){
+                        JOptionPane.showMessageDialog(null, searchedItem + " not found");
+                    }*/
+                setCatalog(customerProfileDatabase.getAllProfiles());
+                }
+
+            });
+        createInvoiceButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+
+            }
+        });
+        leaveButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                closeCatalog();
+            }});
+
+
+        iList.addMouseListener(new MouseAdapter(){
+            public void mouseClicked(MouseEvent evt) {
+                try {
+                    JList jList = (JList) evt.getSource();
+                    if (evt.getClickCount() == 2) {
+                        //index of selected jList is stored and then ItemsGUI displays selectedIndex
+                        /*int index = searchIndex(iList.locationToIndex(evt.getPoint()));
+                        ItemDisplayGUI dis = new ItemDisplayGUI(index);
+                        mainWindowGUI.setJPanel(dis.getPanel());*/
+                    }
+                }
+                catch(ArrayIndexOutOfBoundsException a){
+                    JOptionPane.showMessageDialog(null, "Please make sure you have selected an item.");
+                }
+            }
+        });
+        logoutButton.addActionListener(e ->
+        {
+            mainWindowGUI.setJPanel(new LoginGUI().getPanel());
+        });
+    }
+
+
+    public void setCatalog(CustomerProfile[] v){
+        iList.setListData(v);
+        iList.setFont(new Font("Arial",Font.BOLD,12));
+    }
+    /*
+    public int searchIndex(int index){
+        String itemsString;
+        itemsString = (String) iList.getModel().getElementAt(index);
+        return itemsList.getId(itemsString);
+    }
+    */
+
+    public void closeCatalog(){
+        mainWindowGUI.setJPanel(new MainMenuGUI().getPanel());
+    }
     //methods required for implementing focusListener
     public void focusGained(FocusEvent e) {
         if (e.getSource() instanceof JTextField) {
