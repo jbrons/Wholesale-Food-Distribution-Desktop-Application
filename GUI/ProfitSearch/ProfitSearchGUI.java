d4ee49edbc2a10b3623b00c5d37430cb7e2f2e65
Jacob Price <ga4116@wayne.edu>
2021-04-16 22:34:09 -0400

    basic profit search
    just pushing this so that project compiles for others (whoops)

---
+++ GUI/ProfitSearch/ProfitSearchGUI.java
@@ -0,0 +1,15 @@
+package GUI.ProfitSearch;
+
+import javax.swing.*;
+
+public class ProfitSearchGUI {
+    private JPanel rootPanel;
+    private JPanel profitSearchPanel;
+    private JPanel buttonPanel;
+
+
+    public JPanel getPanel()
+    {
+        return rootPanel;
+    }
+}
